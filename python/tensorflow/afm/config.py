69864e9a2dbe1324f37ab1037066295f95b050dd
Zhiping Wu <zpwu@thoughtworks.com>
2018-06-17 16:11:01 +0800

    Add other algorithm

--- python/tensorflow/afm/config.py
+++ python/tensorflow/afm/config.py
@@ -1,8 +1,7 @@
-TRAIN_FILE = "../../data/train.csv"
-TEST_FILE = "../../data/test.csv"
+TRAIN_FILE = "../data/train.csv"
+TEST_FILE = "../data/test.csv"
 
 SUB_DIR = "output"
-
 
 NUM_SPLITS = 3
 RANDOM_SEED = 2017
--- python/tensorflow/dcn/config.py
+++ python/tensorflow/dcn/config.py
@@ -1,19 +1,18 @@
-TRAIN_FILE = "../../data/train.csv"
-TEST_FILE = "../../data/test.csv"
+TRAIN_FILE = "../data/train.csv"
+TEST_FILE = "../data/test.csv"
 
 SUB_DIR = "output"
-
 
 NUM_SPLITS = 3
 RANDOM_SEED = 2017
 
 # types of columns of the dataset dataframe
 CATEGORICAL_COLS = [
-    # 'ps_ind_02_cat', 'ps_ind_04_cat', 'ps_ind_05_cat',
-    # 'ps_car_01_cat', 'ps_car_02_cat', 'ps_car_03_cat',
-    # 'ps_car_04_cat', 'ps_car_05_cat', 'ps_car_06_cat',
-    # 'ps_car_07_cat', 'ps_car_08_cat', 'ps_car_09_cat',
-    # 'ps_car_10_cat', 'ps_car_11_cat',
+    'ps_ind_02_cat', 'ps_ind_04_cat', 'ps_ind_05_cat',
+    'ps_car_01_cat', 'ps_car_02_cat', 'ps_car_03_cat',
+    'ps_car_04_cat', 'ps_car_05_cat', 'ps_car_06_cat',
+    'ps_car_07_cat', 'ps_car_08_cat', 'ps_car_09_cat',
+    'ps_car_10_cat', 'ps_car_11_cat',
 ]
 
 NUMERIC_COLS = [
